61db6d766857afa3c73dd0ec9e2cd28542d0651d
Khalid Bin Walid <kbwalid26@gmail.com>
2023-11-04 16:37:32 +0900

    update sqrt test program

--- test/sqrt/sqrt.c
+++ test/sqrt/sqrt.c
@@ -1,8 +1,10 @@
-float my_fabs(float x) {
+__attribute__((noinline))
+static float my_fabs(float x) {
     return x < 0 ? -x : x;
 }
 
-float my_sqrt(float x) {
+__attribute__((noinline))
+static float my_sqrt(float x) {
     float out = 1.0f;
     while (my_fabs(out * out - x) / x > 1e-6f) {
         out = (out + x / out) / 2.0f;
